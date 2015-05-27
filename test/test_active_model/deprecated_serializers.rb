79bf60175ed5c023f60376f488d5951bff204a77
Zachary Scott <e@zzak.io>
2015-05-26 17:10:06 -0700

    Add rake/testtask and remove failing test

--- test/test_active_model/deprecated_serializers.rb
+++ test/test_active_model/deprecated_serializers.rb
@@ -1,11 +1,9 @@
 require 'minitest_helper'
 
-class TestActiveModel::DeprecatedSerializers < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::ActiveModel::DeprecatedSerializers::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+module TestActiveModel
+  class DeprecatedSerializers < Minitest::Test
+    def test_that_it_has_a_version_number
+      refute_nil ::ActiveModel::DeprecatedSerializers::VERSION
+    end
   end
 end
