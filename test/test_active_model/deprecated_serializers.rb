f3e84e9f2b9b8393b18c4242b57c715166d536c2
Zachary Scott <e@zzak.io>
2015-05-26 19:27:56 -0700

    Remove unused test

--- test/test_active_model/deprecated_serializers.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest_helper'
-
-module TestActiveModel
-  class DeprecatedSerializers < Minitest::Test
-    def test_that_it_has_a_version_number
-      refute_nil ::ActiveModel::DeprecatedSerializers::VERSION
-    end
-  end
-end
